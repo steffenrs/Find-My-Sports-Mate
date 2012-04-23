a51befb6139f3e2e2382d6ac6d12a0d490a39ffa
Hans Lovland <hanslovland@gmail.com>
2012-04-24 05:02:35 +1000

    Added several domain classes, updated some controller and views

---
+++ DomainLayer/Location.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domain
+{
+    class Location
+    {
+        public string name { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+    }
+}
---
+++ FindMySportsMate/ViewModels/CreateSuggestionViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace PresentationLayer.ViewModels
+{
+    public class CreateSuggestionViewModel
+    {
+    }
+}
