568db1a3a10ee9ea63935628079cae43973b205f
Hans Lovland <hanslovland@gmail.com>
2012-04-24 00:01:15 +1000

    Added SuggestionController and create view

---
+++ FindMySportsMate/Controllers/SuggestionController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Controllers
+{
+    public class SuggestionController : Controller
+    {
+        //
+        // GET: /Suggestion/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
