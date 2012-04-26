de4c64b5ad827da560844728287036c5ac9ae580
Hans Lovland <hanslovland@gmail.com>
2012-04-27 04:32:07 +1000

    added domain exception for throwing internally

---
+++ DomainLayer/DomainException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domain
+{
+    [Serializable()]
+    public class DomainException : System.Exception
+    {
+        public DomainException() : base() { }
+        public DomainException(string message) : base(message) { }
+        public DomainException(string message, System.Exception inner) : base(message, inner) { }
+
+        // A constructor is needed for serialization when an
+        // exception propagates from a remoting server to the client. 
+        protected DomainException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) { }
+    }
+}
