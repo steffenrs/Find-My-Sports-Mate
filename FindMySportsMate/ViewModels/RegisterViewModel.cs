82d257c54f2013adf9660409bd4ba49a20bfd3bd
Simon Stubben <simonstubben@gmail.com>
2012-04-27 05:44:02 +0200

    Changes to User: View
    Added UserBusiness - in progress
    UserController dependent on UserBusiness

--- FindMySportsMate/ViewModels/RegisterViewModel.cs
+++ FindMySportsMate/ViewModels/RegisterViewModel.cs
@@ -8,8 +8,29 @@ using System.Web.Security;
 
 namespace PresentationLayer
 {
-    public class RegisterViewModel
+    public class RegisterViewModel : LogOnViewModel
     {
-        public Domain.RegisterModel RegisterModel { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email address")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Street Address")]
+        public string StreetAddress { get; set; }
+
+        [Required]
+        [Display(Name = "Area")]
+        public string Area { get; set; }
+
+        [Required]
+        [Display(Name = "State")]
+        public string State { get; set; }
     }
 }
