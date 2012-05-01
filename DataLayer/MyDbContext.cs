99c918dc0cecc18a665336d28cdbd58c136afa52
Steffen Stenersen <steffen.stenersen@gmail.com>
2012-05-02 09:43:51 +1000

    --

--- DataLayer/MyDbContext.cs
+++ DataLayer/MyDbContext.cs
@@ -14,6 +14,6 @@ namespace PresentationLayer
         }
 
         public DbSet<AddressBook> Test { get; set; }
-        public DbSet<User> User { get; set; }
+ //       public DbSet<User> User { get; set; }
     }
 }
--- DomainLayer/User.cs
+++ DomainLayer/User.cs
@@ -4,42 +4,41 @@ using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.Web.Mvc;
 using System.Data.Linq;
-using System.Data.Linq.Mapping;
 using System.Web.Security;
 
 namespace Domain
 {
     public class User
     {
-        // TODO add tables and add column attributes
-        [Column(IsPrimaryKey=true, IsDbGenerated=true)]
-        public int Id { get; set; }
-        [Column(CanBeNull=false)]
-        public string UserName { get; set; }
-        [Column(CanBeNull = false)]
-        public string Email { get; set; }
-        [Column(CanBeNull = false)]
-        public string Password { get; set; }
-        [Column]
-        public string StreetAddress { get; set; }
-        [Column]
-        public string Area { get; set; }
-        [Column]
-        public string State { get; set; }
-        [Column]
-        public string FirstName { get; set; }
-        [Column]
-        public string LastName { get; set; }
-        [Column]
-        public bool Gender { get; set; } // true = male, false = female
-        [Column]
-        public DateTime BirthDate { get; set; }
-        [Column]
-        public double Height { get; set; }
-        [Column]
-        public string PhoneNumber { get; set; }
-        [Column] // NHibernate can figure out this itself, but are you able to do it in LINQ - List as a column?
-        public List<Sport> FavoriteSports { get; set; }
+        //// TODO add tables and add column attributes
+        //[Column(IsPrimaryKey=true, IsDbGenerated=true)]
+        //public int Id { get; set; }
+        //[Column(CanBeNull=false)]
+        //public string UserName { get; set; }
+        //[Column(CanBeNull = false)]
+        //public string Email { get; set; }
+        //[Column(CanBeNull = false)]
+        //public string Password { get; set; }
+        //[Column]
+        //public string StreetAddress { get; set; }
+        //[Column]
+        //public string Area { get; set; }
+        //[Column]
+        //public string State { get; set; }
+        //[Column]
+        //public string FirstName { get; set; }
+        //[Column]
+        //public string LastName { get; set; }
+        //[Column]
+        //public bool Gender { get; set; } // true = male, false = female
+        //[Column]
+        //public DateTime BirthDate { get; set; }
+        //[Column]
+        //public double Height { get; set; }
+        //[Column]
+        //public string PhoneNumber { get; set; }
+        //[Column] // NHibernate can figure out this itself, but are you able to do it in LINQ - List as a column?
+        //public List<Sport> FavoriteSports { get; set; }
     }
 
 }
