d2f3fdd5810927cfc75f60197e24bc26ca3882a6
Steffen Stenersen <steffen.stenersen@gmail.com>
2012-05-02 15:21:58 +1000

    dashboard view

--- FindMySportsMate/Controllers/DashboardController.cs
+++ FindMySportsMate/Controllers/DashboardController.cs
@@ -18,6 +18,7 @@ namespace PresentationLayer
             var suggestion = new Suggestion
             {
                 Title = "Volley in winter?",
+                Id=0,
                 Creator = new Domain.User { FirstName = "Frank the tank" },
                 IsClosed = false,
                 Description = "Keen på å spille volleyball eller?",
@@ -30,7 +31,44 @@ namespace PresentationLayer
                 JoinedUsers = new List<JoinedUser> { new JoinedUser { User = new User { FirstName="PETAH" } } }
             };
 
-            var viewModel = new DashboardViewModel() { SelectedSuggestion = suggestion };
+            var suggestion2 = new Suggestion
+            {
+                Title = "Football in winter?",
+                Id=1,
+                Creator = new Domain.User { FirstName = "Frank the ball" },
+                IsClosed = false,
+                Description = "Keen på å spille Football eller?",
+                Sport = new Sport { Name = "Football" },
+                MinimumUsers = 5,
+                MaximumUsers = 10,
+                StartDate = new DateTime(2012, 5, 5),
+                EndDate = new DateTime(2012, 5, 15),
+                Location = new Location { Name = "Footiete" },
+                JoinedUsers = new List<JoinedUser> { new JoinedUser { User = new User { FirstName = "STUBBEN" } } }
+            };
+
+            var suggestion3 = new Suggestion
+            {
+                Title = "Volley in summer?",
+                Id = 2,
+                Creator = new Domain.User { FirstName = "B aks" },
+                IsClosed = true,
+                Description = "Keen på å spille volleyball eller?",
+                Sport = new Sport { Name = "Volleyball" },
+                MinimumUsers = 5,
+                MaximumUsers = 10,
+                StartDate = new DateTime(2012, 5, 5),
+                EndDate = new DateTime(2012, 5, 15),
+                Location = new Location { Name = "Bak deg" },
+                JoinedUsers = new List<JoinedUser> { new JoinedUser { User = new User { FirstName = "han andre" } } }
+            };
+
+            var viewModel = new DashboardViewModel() 
+            { 
+                AllSuggestions = new List<Suggestion> { suggestion, suggestion2, suggestion3 }, 
+                SelectedSuggestion = suggestion,
+                JoinedSuggestions = new List<Suggestion>()
+            };
 
             return View(viewModel);
         }
