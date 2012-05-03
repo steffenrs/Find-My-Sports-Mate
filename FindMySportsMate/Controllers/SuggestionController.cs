4da0958525fea94248698c4e4cd6d1de3d9e3b50
Hans Lovland <hanslovland@gmail.com>
2012-05-04 05:12:38 +1000

    edit suggestion and CRUD methods

--- FindMySportsMate/Controllers/SuggestionController.cs
+++ FindMySportsMate/Controllers/SuggestionController.cs
@@ -21,27 +21,11 @@ namespace PresentationLayer.Controllers
         {
             if (ModelState.IsValid)
             {
-                Sport suggestionSport;
-                try { suggestionSport = BusinessLayer.SportBusiness.GetByName(model.Sport); }
-                catch (DomainException e) { suggestionSport = new Sport { Name = model.Sport }; }
-
-                User currentUser = new User { Id = 1 };
-       
-                Suggestion suggestion = new Suggestion
-                {
-                    Title = model.Title,
-                    StartDate = model.StartDate,
-                    EndDate = model.EndDate,
-                    Description = model.Description,
-                    MinimumUsers = model.MinPeople,
-                    MaximumUsers = model.MaxPeople,
-                    SportId = suggestionSport.Name,
-                    CreatorId = currentUser.Id,
-                };
+                Suggestion suggestion = getDomainFromViewModel(model);
                 
                 try
                 {
-                    BusinessLayer.SuggestionBusiness.New(suggestion);
+                    BusinessLayer.SuggestionBusiness.Create(suggestion);
                     return RedirectToAction("Index", "Dashboard");
                 }
                 catch (DomainException e)
@@ -56,36 +40,90 @@ namespace PresentationLayer.Controllers
             }
         }
 
+        [HttpGet]
+        public ActionResult Edit(int Id)
+        {
+            try
+            {
+                Suggestion suggestion = BusinessLayer.SuggestionBusiness.Read(Id);
+                EditSuggestionViewModel viewModel = new EditSuggestionViewModel
+                {
+                    Title = suggestion.Title,
+                    Sport = suggestion.Sport.Name,
+                    StartDate = suggestion.StartDate,
+                    EndDate = suggestion.EndDate,
+                    Description = suggestion.Description,
+                    MinPeople = suggestion.MinimumUsers,
+                    MaxPeople = suggestion.MaximumUsers,
+                    OriginalSuggestion = suggestion
+                };
+
+                return View("Create", viewModel);
+            }
+            catch (DomainException e) { return View("Error"); } // didnt find suggestion 404
+        }
+
+        [HttpPost]
+        public ActionResult Edit(EditSuggestionViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Suggestion suggestion = getDomainFromViewModel(model);
+                suggestion.Id = model.OriginalSuggestion.Id;
+                suggestion.Location = model.OriginalSuggestion.Location;
+                suggestion.JoinedUsers = model.OriginalSuggestion.JoinedUsers;
+                suggestion.IsClosed = model.OriginalSuggestion.IsClosed;
+
+                try
+                {
+                    BusinessLayer.SuggestionBusiness.Update(suggestion);
+                    return RedirectToAction("Index", "Dashboard");
+                }
+                catch (DomainException e)
+                {
+                    ViewBag.ExceptionMessage = e.Message;
+                    return View(model);
+                }
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+
+
+
+        private Suggestion getDomainFromViewModel(CreateSuggestionViewModel model)
+        {
+            //Fetch user info
+            User currentUser = new User { Id = 1 };
+
+            // Fetch sport
+            Sport suggestionSport;
+            try { suggestionSport = BusinessLayer.SportBusiness.GetByName(model.Sport); }
+            catch (DomainException e) { suggestionSport = new Sport { Name = model.Sport }; }
+
+            Suggestion suggestion = new Suggestion
+            {
+                Title = model.Title,
+                StartDate = model.StartDate,
+                EndDate = model.EndDate,
+                Description = model.Description,
+                MinimumUsers = model.MinPeople,
+                MaximumUsers = model.MaxPeople,
+                SportId = suggestionSport.Name,
+                CreatorId = currentUser.Id,
+            };
+
+            return suggestion;
+        }
+
         public JsonResult GetSuggestion(int id)
         {
             Suggestion suggestion = BusinessLayer.SuggestionBusiness.Get(id);
 
             return Json(suggestion, JsonRequestBehavior.AllowGet);
-        }
-
-     
-
-        [HttpGet]
-        public ActionResult Edit(int Id)
-        {
-            Suggestion suggestion;
-            try { suggestion = BusinessLayer.SuggestionBusiness.Get(Id); }
-            catch (DomainException e) { return null; }
-            
-            CreateSuggestionViewModel viewModel = new CreateSuggestionViewModel {   Title = suggestion.Title, 
-                                                                                    Sport = suggestion.Sport.Name, 
-                                                                                    StartDate = suggestion.StartDate, 
-                                                                                    EndDate = suggestion.EndDate, 
-                                                                                    Description = suggestion.Description, 
-                                                                                    MinPeople = suggestion.MinimumUsers, 
-                                                                                    MaxPeople = suggestion.MaximumUsers };
-            return View("Create", viewModel);
-        }
-
-        [HttpPost]
-        public ActionResult Edit(CreateSuggestionViewModel model)
-        {
-            return null;
         }
     }
 }
