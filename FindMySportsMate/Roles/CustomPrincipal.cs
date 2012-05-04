6e8a617eed687934c65047fd7b8a7a3e241b8760
Steffen Stenersen <steffen.stenersen@gmail.com>
2012-05-04 13:00:53 +1000

    added custom principal

---
+++ FindMySportsMate/Roles/CustomPrincipal.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Security;
+using System;
+using System.Security.Principal;
+
+namespace PresentationLayer
+{
+
+    class CustomPrincipal : RolePrincipal
+    {
+        public Guid UserID { get; set; }
+
+        public CustomPrincipal(String name, IIdentity provider, Guid userID)
+            : base(name, provider)
+        {
+            UserID = userID;
+        }
+    }
+}
