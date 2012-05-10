c10e59219830cb4c641c35ea04adee7aeae34052
Hans Lovland <hanslovland@gmail.com>
2012-05-11 02:48:42 +1000

    gmatrix..

---
+++ BusinessLayer/GMatrix.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Web;
+using Domain;
+
+namespace BusinessLayer
+{
+    public class GMatrix
+    {
+        private static string API_KEY = "";
+
+        public static Location nearestLocationForUsers(Suggestion suggestion)
+        {
+
+
+
+
+
+
+            
+
+            return null;
+        }
+
+
+
+
+
+
+
+        public static int getDistanceBetweenUserAddressAndLocation(User user, Location location)
+        {
+            var url = "http://maps.googleapis.com/maps/api/distancematrix/json?origins=" +
+                        HttpUtility.UrlEncode(address) +
+                        "&destinations=" + to +
+                        "&mode=driving&language=en&sensor=false";
+
+            var request = WebRequest.Create(url);
+            var response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+
+            }
+            
+
+
+
+            return 0;
+        }
+    }
+}
