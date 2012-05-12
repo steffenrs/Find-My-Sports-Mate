181fe98ab7dd0cc3c2ae175046b6161d36e763ef
Steffen Stenersen <steffen.stenersen@gmail.com>
2012-05-12 22:04:49 +1000

    removed </br> text from validation message

--- FindMySportsMate/ViewModels/LogOnViewModel.cs
+++ FindMySportsMate/ViewModels/LogOnViewModel.cs
@@ -16,7 +16,7 @@ namespace PresentationLayer
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "<br/>The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
