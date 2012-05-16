d98b810d85f514bb18e7f43f9289c64d32719eff
Simon Stubben <simonstubben@gmail.com>
2012-05-16 05:03:59 +0200

    GoogleJsonObjects..

---
+++ DomainLayer/GoogleJsonObject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Dynamic;
+using System.Collections;
+
+namespace Domain
+{
+    public class Distance
+    {
+        public string text { get; set; }
+        public int value { get; set; }
+    }
+
+    public class Duration
+    {
+        public string text { get; set; }
+        public int value { get; set; }
+    }
+
+    public class Element
+    {
+        public Distance distance { get; set; }
+        public Duration duration { get; set; }
+        public string status { get; set; }
+    }
+
+    public class RootObject
+    {
+        public List<Element> elements { get; set; }
+    }
+
+    
+}
