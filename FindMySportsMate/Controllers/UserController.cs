9417e0ca609b1ea84345b08712570f7338a31d31
Simon Stubben <simonstubben@gmail.com>
2012-05-22 05:25:15 +0200

    User Profile View

--- FindMySportsMate/Controllers/UserController.cs
+++ FindMySportsMate/Controllers/UserController.cs
@@ -14,7 +14,39 @@ namespace PresentationLayer
 {
     public class UserController : Controller
     {
+         //
+        // GET: /Dashboard/
+        [CustomAuthorize]
+        public ActionResult Profile(int Id)
+        {
+            try
+            {
+                User user = BusinessLayer.UserBusiness.GetUserById(Id);
+                UserViewModel viewModel = new UserViewModel
+                {
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Gender = user.Gender,
+                    BirthDate = user.BirthDate,
+                    Email = user.Email,
+                    StreetAddress = user.StreetAddress,
+                    Area = user.Area,
+                    State = user.State,
+                    PhoneNumber = user.PhoneNumber
+                };
 
+                return View(viewModel);
+            }
+            catch (DomainException e)
+            {
+                return View("Error");
+            }
+            catch (Exception e)
+            {
+                return View("Error");
+            }
+        }
+        
         // GET: /Account/LogOn
         public ActionResult LogOn()
         {
